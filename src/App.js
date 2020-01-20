e795e20d9e9ab2e469fc329b1fb13757b25b3e1c
Devon <devon@Devons-MacBook-Pro.local>
2020-01-20 10:36:14 +0200

    Added add assets and styled them

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="login-page">
+        <div className="typography">
+          <h1>Learn to code by watching others</h1>
+          <p>See how experienced developers solve problems in real time. Watching scripted tutorials is great, but understanding how developers think is invaluable</p>
+        </div>
+        <div className="form-wrapper">
+          <button className="submit-green try-button"><strong>Try it free 7 days</strong> then $20/mo. thereafter</button>
+          <div className="white-form">
+            <form>
+              <input placeholder="First Name" type="text" name="name" />     
+              <input placeholder="Last Name" type="text" name="name" />
+              <input placeholder="Email" type="text" name="name" />
+              <input placeholder="Password" type="text" name="name" />
+              <input className="submit-green" type="submit" value="Claim Your Free Trial" />
+            </form>
+            <p id="terms-services">By clicking the button, you are agreeing to our <a id="red-terms-link">Terms and Services</a></p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
